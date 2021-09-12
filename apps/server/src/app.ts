9177fbe2b7897897d6f99bd17043ded642fe24fd
tung <tungnguyen@yojee.com>
2021-09-12 16:21:35 +0700

    update: script to run server

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -48,7 +48,7 @@ export async function makeApp({
 
   app.use((req, res, next) => {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.NEXT_PUBLIC_CFS_URL);
+    res.setHeader('Access-Control-Allow-Origin', process.env.NEXT_PUBLIC_CFS_URL || '');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
