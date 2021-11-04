99d5c10486fe2236095dc29191ff3763a40ee197
tung <tungnguyen@yojee.com>
2021-11-04 09:47:36 +0700

    fix show login dialog when comment

--- apps/client/components/Header/HeaderUI.js
+++ apps/client/components/Header/HeaderUI.js
@@ -1,58 +1,21 @@
 import React from 'react';
-import dynamic from 'next/dynamic';
 import { Box } from '@chakra-ui/react';
-import { useReactiveVar } from '@apollo/react-hooks';
-import { showFeedbacksModal } from '@cfs/helper';
-import {
-  showLoginPopup,
-  showRegisterPopup,
-  showPromoteLoginOrRegisterPopup,
-} from '@cfs/helper';
 import CreateNewCfs from './CreateNewCfs';
 import CustomMenu from './CustomMenu';
-import { Loading } from '@cfs/ui';
-
-const RegisterPopup = dynamic(() => import('../RegisterPopup'), {
-  loading: () => <Loading />,
-});
-const FeedbacksModal = dynamic(() => import('../Feedbacks/FeedbacksModal'), {
-  loading: () => <Loading />,
-});
-const LoginPopup = dynamic(() => import('../LoginPopup'), {
-  loading: () => <Loading />,
-});
-const PromoteLoginOrRegister = dynamic(
-  () => import('../PromoteLoginOrRegister'),
-  { loading: () => <Loading /> }
-);
 
 const HeaderUI = () => {
-  const loginPopupVisible = useReactiveVar(showLoginPopup);
-  const registerPopupVisible = useReactiveVar(showRegisterPopup);
-  const feedbacksModalVisible = useReactiveVar(showFeedbacksModal);
-  const promoteLoginOrRegisterPopupVisible = useReactiveVar(
-    showPromoteLoginOrRegisterPopup
-  );
-
   return (
-    <>
-      <nav className="header-height select-none header-shadow">
-        <div className="flex justify-between items-center h-full mx-1 sm:mx-6">
-          <CustomMenu />
-          <Box ml={4} mr={4} fontStyle="italic">
-            Nơi bạn chia sẻ những tâm sự thầm kín
-          </Box>
-          <Box display="flex">
-            <CreateNewCfs />
-          </Box>
-        </div>
-      </nav>
-
-      {loginPopupVisible && <LoginPopup />}
-      {registerPopupVisible && <RegisterPopup />}
-      {feedbacksModalVisible && <FeedbacksModal />}
-      {promoteLoginOrRegisterPopupVisible && <PromoteLoginOrRegister />}
-    </>
+    <nav className="header-height select-none header-shadow">
+      <div className="flex justify-between items-center h-full mx-1 sm:mx-6">
+        <CustomMenu />
+        <Box ml={4} mr={4} fontStyle="italic">
+          Nơi bạn chia sẻ những tâm sự thầm kín
+        </Box>
+        <Box display="flex">
+          <CreateNewCfs />
+        </Box>
+      </div>
+    </nav>
   );
 };
 
--- apps/client/components/Layout/index.js
+++ apps/client/components/Layout/index.js
@@ -1,9 +1,39 @@
 import React, { useEffect } from 'react';
-import { setCurrentUser } from '@cfs/helper';
+import {
+  setCurrentUser,
+  showFeedbacksModal,
+  showLoginPopup,
+  showPromoteLoginOrRegisterPopup,
+  showRegisterPopup,
+} from '@cfs/helper';
 import { useSharedLazyQuery } from '@cfs/graphql';
+import { useReactiveVar } from '@apollo/react-hooks';
+import dynamic from 'next/dynamic';
+import { Loading } from '@cfs/ui';
+
+const RegisterPopup = dynamic(() => import('../RegisterPopup'), {
+  loading: () => <Loading />,
+});
+const FeedbacksModal = dynamic(() => import('../Feedbacks/FeedbacksModal'), {
+  loading: () => <Loading />,
+});
+const LoginPopup = dynamic(() => import('../LoginPopup'), {
+  loading: () => <Loading />,
+});
+const PromoteLoginOrRegister = dynamic(
+  () => import('../PromoteLoginOrRegister'),
+  { loading: () => <Loading /> }
+);
 
 const MainLayout = ({ children }) => {
   const [getShareData, { data: shareData }] = useSharedLazyQuery();
+
+  const loginPopupVisible = useReactiveVar(showLoginPopup);
+  const registerPopupVisible = useReactiveVar(showRegisterPopup);
+  const feedbacksModalVisible = useReactiveVar(showFeedbacksModal);
+  const promoteLoginOrRegisterPopupVisible = useReactiveVar(
+    showPromoteLoginOrRegisterPopup
+  );
 
   useEffect(() => {
     getShareData();
@@ -14,9 +44,14 @@ const MainLayout = ({ children }) => {
   }, [shareData?.currentUser]);
 
   return (
-    <div className="max-w-screen-xl mx-auto my-0">
-      {children}
-    </div>
+    <>
+      <div className="max-w-screen-xl mx-auto my-0">{children}</div>
+
+      {loginPopupVisible && <LoginPopup />}
+      {registerPopupVisible && <RegisterPopup />}
+      {feedbacksModalVisible && <FeedbacksModal />}
+      {promoteLoginOrRegisterPopupVisible && <PromoteLoginOrRegister />}
+    </>
   );
 };
 
