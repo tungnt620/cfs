08f5683e20cd698d40e4ba9fa1ee3df4d2da914a
tung <tungnguyen@yojee.com>
2021-11-25 20:42:55 +0700

    update: change sub sitemap file name

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,5 +1,9 @@
 const withNx = require('@nrwl/next/plugins/with-nx');
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 const nextConfig = {
   images: {
     domains: ['confession.vn', 'storage.googleapis.com'],
@@ -7,5 +11,4 @@ const nextConfig = {
   trailingSlash: true,
 };
 
-// module.exports = withPreact(withNx(nextConfig));
-module.exports = withNx(nextConfig);
+module.exports = withNx(withBundleAnalyzer(nextConfig));
