63dbeef5481ab0300af1c76802d420e112420245
tung <tungnguyen@yojee.com>
2021-11-25 20:56:37 +0700

    update: change sub sitemap file name

--- apps/worker/src/tasks/generate_sitemap.js
+++ apps/worker/src/tasks/generate_sitemap.js
@@ -158,7 +158,7 @@ function generate(urls, logger) {
       // if your server automatically serves sitemap.xml.gz when requesting sitemap.xml leave this line be
       // otherwise you will need to add .gz here and remove it a couple lines below so that both the index
       // and the actual file have a .gz extension
-      const path = `${basePath}/sitemap-${i}.xml.gz`;
+      const path = `./sitemap-${i}.xml.gz`;
 
       sitemapStream
         .pipe(createGzip()) // compress the output of the sitemap
