8fe73f8dc35fcd53a618980f86b8669550ec820b
tung <tungnguyen@yojee.com>
2021-11-28 11:56:11 +0700

    fix: stop redirect in images url

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -3,6 +3,7 @@ const withNx = require('@nrwl/next/plugins/with-nx');
 const nextConfig = {
   images: {
     domains: ['confession.vn', 'storage.googleapis.com'],
+    path: '/_next/image/',
   },
   trailingSlash: true,
   // productionBrowserSourceMaps: true,
