69eda23e05aae8188b33c2aad9586bcfffe08308
tung <tungnguyen@yojee.com>
2021-12-29 11:43:32 +0700

    feature: allow create new community

--- apps/server/ecosystem.config.js
+++ apps/server/ecosystem.config.js
@@ -8,6 +8,7 @@ module.exports = {
     instances: "1",
     autorestart: true,
     watch: false,
+    exec_mode: 'fork',
     ignore_watch : ["node_modules"],
     max_memory_restart: '1G',
     env_production: {
