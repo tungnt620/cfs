b22e0655ee0e5255d4aaedd010de803380997e96
tung <tungnguyen@yojee.com>
2021-12-29 11:35:28 +0700

    feature: allow create new community

--- apps/server/src/middleware/installHelmet.ts
+++ apps/server/src/middleware/installHelmet.ts
@@ -1,7 +1,7 @@
 import { Express } from "express";
 import helmet from "helmet";
 
-const tmpRootUrl = 'http://localhost:5678'
+const tmpRootUrl = process.env.ROOT_URL;
 
 if (!tmpRootUrl || typeof tmpRootUrl !== "string") {
   throw new Error("Envvar ROOT_URL is required.");
