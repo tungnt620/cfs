b05b5f1fa477136b1d55825b60eb3beeef7af0ae
Tung <tungnguyen@yojee.com>
2022-03-27 21:49:35 +0700

    fix: undefined file object

--- libs/helper/src/gcs/uploadFile.js
+++ libs/helper/src/gcs/uploadFile.js
@@ -1,11 +1,13 @@
 import { getSignedUrl, uploadFileWithSignedUrl } from '@cfs/api/gcs';
 
 const uploadFile = async (file) => {
-  const fileExt = file.type.split('/')[1];
-  const { signedUrl, publicUrl } = await getSignedUrl(fileExt);
-  const result = await uploadFileWithSignedUrl(signedUrl, file);
+  if (file) {
+    const fileExt = file.type.split('/')[1];
+    const { signedUrl, publicUrl } = await getSignedUrl(fileExt);
+    const result = await uploadFileWithSignedUrl(signedUrl, file);
 
-  return result ? publicUrl : null;
+    return result ? publicUrl : null;
+  }
 };
 
 export default uploadFile;
