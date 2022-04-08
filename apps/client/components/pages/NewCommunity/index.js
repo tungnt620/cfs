2ec20c34d1f1ad87a9b8cbec438ee5236908ced2
Tung <tungnguyen@yojee.com>
2022-04-08 23:49:09 +0700

    fix: limit file size upload to google

--- apps/client/components/pages/NewCommunity/index.js
+++ apps/client/components/pages/NewCommunity/index.js
@@ -93,12 +93,12 @@ const NewCommunityPage = () => {
     const { image: bannerImage } = values.banner_image || {};
     if (bannerImage) {
       bannerImageUrl = await uploadFile(bannerImage, toast);
-      if (image === 'error') return;
+      if (bannerImageUrl === 'error') return;
     }
     const { image: profileImage } = values.image || {};
     if (profileImage) {
       profileImageUrl = await uploadFile(profileImage, toast);
-      if (image === 'error') return;
+      if (profileImageUrl === 'error') return;
     }
 
     if (bannerImageUrl && profileImageUrl) {
